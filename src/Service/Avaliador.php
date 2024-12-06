ec8251d2e42f12a60233ab32a7d547632fbf7485
EdsonJunio <edsonbh97@gmail.com>
2024-12-06 20:11:27 -0300

    Refactor tests and improve bid evaluation logic
    Reorganize and enhance test cases for bid evaluations, introducing data providers for consistent test data management. Added validation to prevent evaluations of empty auctions and updated bid rules in the auction model to limit consecutive bids by the same user and a maximum number of bids per user.

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -13,6 +13,9 @@ class Avaliador
 
     public function avalia(Leilao $leilao)
     {
+        if(empty($leilao->getLances())) {
+            throw new \DomainException('Não e possível avaliar leilão vazio');
+        }
         foreach ($leilao->getLances() as $lance) {
             if ($lance->getValor() > $this->maiorValor) {
                 $this->maiorValor = $lance->getValor();
@@ -22,9 +25,10 @@ class Avaliador
                 $this->menorValor = $lance->getValor();
             }
         }
+
         $lances = $leilao->getLances();
-        usort($lances, function (Lance $lances1, Lance $lances2) {
-            return $lances2->getValor() -  $lances1->getValor();
+        usort($lances, function (Lance $lance1, Lance $lance2) {
+            return $lance2->getValor() - $lance1->getValor();
         });
         $this->maioresLances = array_slice($lances, 0, 3);
     }
@@ -39,6 +43,9 @@ class Avaliador
         return $this->menorValor;
     }
 
+    /**
+     * @return Lance[]
+     */
     public function getMaioresLances(): array
     {
         return $this->maioresLances;
